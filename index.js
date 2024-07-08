a4183636a40831958a6ecebd8b055a08df860655
Nosvetra <goludhruvkumar@gmail.com>
2024-07-08 22:25:20 +0530

    Initail Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const button = document.getElementById("to-add-photo");
+const sacredButton = document.getElementById("to-add-photo2");
+button.addEventListener("click", () => {
+  const xttp = new XMLHttpRequest();
+  xttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let responseObj = JSON.parse(this.responseText);
+      const img = document.getElementsByClassName("asyncImg")[0];
+      img.src = responseObj.url;
+      //   console.log(responseObj);
+    }
+  };
+  xttp.open("GET", "https://api.waifu.pics/sfw/waifu", true);
+  xttp.send();
+});
+
+sacredButton.addEventListener("click", () => {
+  const url = "https://api.waifu.pics/nsfw/waifu";
+  const response = fetch(url, "GET");
+  fetch(url)
+    .then((response) => response.json())
+    .then((ojj) => {
+      const img2 = document.getElementsByClassName("asyncImg2")[0];
+      img2.src = ojj["url"];
+    })
+    .catch((err) => console.error(err));
+});
