eb02a8e7af16ae4897fed13452cb0506a42fdcdc
Nosvetra <goludhruvkumar@gmail.com>
2024-07-15 00:42:06 +0530

    changes on ui

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const button = document.getElementById("to-add-photo");
 const sacredButton = document.getElementById("to-add-photo2");
 button.addEventListener("click", () => {
+  const img = document.getElementsByClassName("asyncImg")[0];
   const xttp = new XMLHttpRequest();
   xttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
+      img.style.display = "block";
       let responseObj = JSON.parse(this.responseText);
-      const img = document.getElementsByClassName("asyncImg")[0];
       img.src = responseObj.url;
     }
   };
@@ -14,12 +15,12 @@ button.addEventListener("click", () => {
 });
 
 sacredButton.addEventListener("click", () => {
+  const img2 = document.getElementsByClassName("asyncImg2")[0];
   const url = "https://api.waifu.pics/nsfw/waifu";
-  const response = fetch(url, "GET");
-  fetch(url)
+  fetch(url, { method: "get" })
     .then((response) => response.json())
     .then((ojj) => {
-      const img2 = document.getElementsByClassName("asyncImg2")[0];
+      img2.style.display = "block";
       img2.src = ojj["url"];
     })
     .catch((err) => console.error(err));
